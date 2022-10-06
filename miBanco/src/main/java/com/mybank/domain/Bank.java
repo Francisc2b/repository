cba2aa946962315d68379436795769def8bb59a0
Francis <capitanjackh@yahoo.com>
2022-10-06 23:27:14 +0200

    bank3

---
+++ miBanco/src/main/java/com/mybank/domain/Bank.java
@@ -0,0 +1,46 @@
+package com.mybank.domain;
+
+public class Bank {
+	
+
+	
+	private  Customer[] customers;
+	private  int numberOfCustomers;
+	
+	
+	public Bank() {
+		customers = new Customer[3];
+		numberOfCustomers = 0;
+		}
+	
+	public Bank(Customer[] customers, int numberCustomers) {
+		super();
+		this.customers = customers;
+		this.numberOfCustomers = numberCustomers;
+	}
+	
+	public Customer[] getCustomers() {
+		return customers;
+	}
+	
+	public void setCustomers(Customer[] customers) {
+		this.customers = customers;
+	}
+	
+	public int getNumberOfCustomerss() {
+		return numberOfCustomers;
+	}
+	
+	public void setNumberOfCustomers(int numberCustomers) {
+		this.numberOfCustomers = numberCustomers;
+	}
+	
+	public void addCustomer(String firstName, String lastName ) {
+		if (numberOfCustomers <= customers.length) {
+			customers[numberOfCustomers] = new Customer(firstName,lastName);
+			numberOfCustomers ++;
+		}
+	}
+	
+
+}
