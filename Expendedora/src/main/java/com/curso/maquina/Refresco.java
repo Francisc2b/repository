8b030071dc07c5c7d754d271dd0fa23aa30b82b3
Francisc2b <114911191+Francisc2b@users.noreply.github.com>
2022-10-06 13:22:35 +0200

    inicio

---
+++ Expendedora/src/main/java/com/curso/maquina/Refresco.java
@@ -0,0 +1,12 @@
+package com.curso.maquina;
+
+public class Refresco {
+	
+	private int id;
+	private String sabor;
+	private int cl;
+	private double precio;
+	private String agotado;
+	private int stock;
+
+}
